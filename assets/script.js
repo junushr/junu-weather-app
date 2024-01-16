691eb6b0dfefc1abc37f6db0451efebdec9cfbae
Junu Shrestha <junu.shrestha.uk@gmail.com>
2024-01-16 22:40:51 +0000

    Boilerplate code added

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Global variables
+const apiKey = '30ced98bf7b47a3e0d5002206004e34b';
+
+
+// Function to fetch weather data from Openweather API
+
+
+// Function to display the current weather of a city
+
+// Function to display the 5-day forecast of the same city displayed in displayCurrentWeather()
+
+    // Create elements
+
+
+    // Print elements to page
+
+
+// Function to add searched locations to local storage
+
+
+// Function to display locations previously searched
+
+// Event listener on search button
+
+// If searchHistory exists in localstorage, render search history to page on page load
